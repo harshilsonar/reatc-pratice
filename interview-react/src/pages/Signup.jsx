4061848cf003afb97ceff6ce65f9f6dc57f870b9
harshilsonar <sonarhet8426@gmail.com>
2026-06-29 11:57:05 +0530

    update signup page

--- interview-react/src/pages/Signup.jsx
+++ interview-react/src/pages/Signup.jsx
@@ -1,49 +1,100 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Signup = () => {
-     return (
+  const [user, setuser] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  // Handle Input Change
+ const handlechange =()=>{
+   setuser({
+    ...user
+    [e.target.name].e.target.value,
+   });
+ };
+
+ const handlesubmit = (e) => {
+    e.preventdefault()
+    console.log(user)
+
+    setuser({
+        name:"",
+        email:"",
+        password:""
+    });
+ };
+  
+
+  return (
     <div className="min-h-screen bg-gray-100 flex justify-center items-center">
       <div className="w-96 bg-white border border-gray-300 rounded-xl shadow-lg p-8">
         <h1 className="text-3xl font-bold text-center mb-6">
           Sign Up
         </h1>
 
-        <div className="mb-4">
-          <label className="block mb-2 font-medium">
-            Name
-          </label>
-          <input
-            type="text"
-            placeholder="Enter your name"
-            className="w-full border border-gray-400 rounded-lg p-2 focus:outline-none focus:border-blue-500"
-          />
-        </div>
+        <form onSubmit={handlesubmit}>
+          {/* Name */}
 
-        <div className="mb-4">
-          <label className="block mb-2 font-medium">
-            Email
-          </label>
-          <input
-            type="email"
-            placeholder="Enter your email"
-            className="w-full border border-gray-400 rounded-lg p-2 focus:outline-none focus:border-blue-500"
-          />
-        </div>
+          <div className="mb-4">
+            <label className="block mb-2 font-medium">
+              Name
+            </label>
 
-        <div className="mb-6">
-          <label className="block mb-2 font-medium">
-            Password
-          </label>
-          <input
-            type="password"
-            placeholder="Enter your password"
-            className="w-full border border-gray-400 rounded-lg p-2 focus:outline-none focus:border-blue-500"
-          />
-        </div>
+            <input
+              type="text"
+              placeholder="Enter your name"
+              name="name"
+              value={user.name}
+              onChange={handlechange}
+              className="w-full border border-gray-400 rounded-lg p-2 focus:outline-none focus:border-blue-500"
+            />
+          </div>
 
-        <button className="w-full bg-black text-white py-2 rounded-lg hover:bg-gray-800 transition">
-          Sign Up
-        </button>
+          {/* Email */}
+
+          <div className="mb-4">
+            <label className="block mb-2 font-medium">
+              Email
+            </label>
+
+            <input
+              type="email"
+              placeholder="Enter your email"
+              name="email"
+              value={user.email}
+              onChange={handlechange}
+              className="w-full border border-gray-400 rounded-lg p-2 focus:outline-none focus:border-blue-500"
+            />
+          </div>
+
+          {/* Password */}
+
+          <div className="mb-6">
+            <label className="block mb-2 font-medium">
+              Password
+            </label>
+
+            <input
+              type="password"
+              placeholder="Enter your password"
+              name="password"
+              value={user.password}
+              onChange={handlechange}
+              className="w-full border border-gray-400 rounded-lg p-2 focus:outline-none focus:border-blue-500"
+            />
+          </div>
+
+          {/* Button */}
+
+          <button
+            type="submit"
+            className="w-full bg-black text-white py-2 rounded-lg hover:bg-gray-800 transition"
+          >
+            Sign Up
+          </button>
+        </form>
 
         <p className="text-center mt-5 text-gray-600">
           Already have an account?
@@ -54,6 +105,5 @@ const Signup = () => {
       </div>
     </div>
   );
-}
-
-export default Signup
+};
+export default Signup;
